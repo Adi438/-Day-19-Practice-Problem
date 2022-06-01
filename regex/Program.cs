e2987d1ed164d0d0e9a43a9f2cae27874fafa695
Adi438 <mishraaaditya54@gmail.com>
2022-06-01 22:57:23 +0530

    first name code adding

--- regex/Program.cs
+++ regex/Program.cs
@@ -1,15 +1,28 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
-namespace regex
+namespace RegexProblems
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("\n\n----------------WELCOME TO THE REGULAR EXPRESSION (REGEX) PROGRAM------------------------\n\n");
+            while (true)
+            {
+                Console.WriteLine("Enter Your First Name : ");
+                string name = Console.ReadLine();
+                string nm = "^[A-Z][a-z]{2,}$";
+                if (Regex.IsMatch(name, nm))
+                {
+                    Console.WriteLine("Your Name is : " + name);
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Please Enter First name start with Capital and minimum Three characters : " + false);
+                }
+            }
         }
     }
 }

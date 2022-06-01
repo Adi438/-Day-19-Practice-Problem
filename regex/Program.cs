206d2d5b64184f80e8b5bcd4c57313ce6840701a
Adi438 <mishraaaditya54@gmail.com>
2022-06-01 22:59:16 +0530

    adding uc2 last name code

--- regex/Program.cs
+++ regex/Program.cs
@@ -10,17 +10,17 @@ namespace RegexProblems
             Console.WriteLine("\n\n----------------WELCOME TO THE REGULAR EXPRESSION (REGEX) PROGRAM------------------------\n\n");
             while (true)
             {
-                Console.WriteLine("Enter Your First Name : ");
+                Console.WriteLine("Enter Your Last Name : ");
                 string name = Console.ReadLine();
                 string nm = "^[A-Z][a-z]{2,}$";
                 if (Regex.IsMatch(name, nm))
                 {
-                    Console.WriteLine("Your Name is : " + name);
+                    Console.WriteLine("Your Last is : " + name);
                     break;
                 }
                 else
                 {
-                    Console.WriteLine("Please Enter First name start with Capital and minimum Three characters : " + false);
+                    Console.WriteLine("Please Enter Last name start with Capital and minimum Three characters : " + false);
                 }
             }
         }
